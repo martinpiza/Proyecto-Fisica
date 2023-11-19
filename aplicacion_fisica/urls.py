738226c3f288ac0a7f030610565c2add26db14ce
martinpiza <martinpiza19@gmail.com>
2023-11-18 19:08:32 -0500

    Se agregan cambios a las calculadoras

--- aplicacion_fisica/urls.py
+++ aplicacion_fisica/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, movimiento_circular, fuerza, leyes_newton
+from .views import index, movimiento_circular, fuerza, leyes_newton, inicio
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('/', index, name='index'),
+    path('', inicio, name='inicio'),
     path('movimiento-circular/', movimiento_circular, name='movimiento_circular'),
     path('fuerza/', fuerza, name='fuerza'),
     path('leyes-newton/', leyes_newton, name='leyes_newton'),
